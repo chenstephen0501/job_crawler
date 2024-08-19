4ad23522183ff08fbf2a1d793b41e5f7f1a30d06
stephen0501 <tom9876555@gmail.com>
2024-08-19 10:33:43 +0800

    feat user_apply_jobs route and API

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import fetch_cake_jobs, fetch_cake_jobs_all
+from .views import fetch_cake_jobs, fetch_cake_jobs_all, user_apply_jobs
 
 app_name = "jobs"
 
 urlpatterns = [
     path("fetch_cake/", fetch_cake_jobs, name="fetch_cake_jobs"),
-    path("fetch_cake_all/", fetch_cake_jobs_all, name="fetch_cake_jobs_all")
+    path("fetch_cake_all/", fetch_cake_jobs_all, name="fetch_cake_jobs_all"),
+    path("user_apply_jobs/", user_apply_jobs, name="user_apply_jobs")
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-import requests
+import requests, logging, time, json, uuid, os
 from bs4 import BeautifulSoup
 from django.http import JsonResponse
-
-import logging, time, json, uuid
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 logging = logging.getLogger("django")
 
@@ -99,7 +101,7 @@ def fetch_cake_jobs_all(req):
                     salary = 'N/A'
 
                 position_element = job_item.find('a', class_='JobSearchItem_featureSegmentLink__yMxQc')
-                
+
                 if position_element:
                     position_location = position_element.text.strip()
                 else:
@@ -134,3 +136,85 @@ def fetch_cake_jobs_all(req):
             break
 
     return JsonResponse(jobs, safe=False)
+
+def user_apply_jobs(request):
+    url = 'https://www.cakeresume.com/'
+    firefox_binary_path = 'C:/Program Files/Mozilla Firefox/firefox.exe'
+    options = webdriver.FirefoxOptions()
+    options.binary_location = firefox_binary_path
+    geckodriver_path = "C:/webdriver/geckodriver-v0.35.0-win64/geckodriver.exe"
+    service = webdriver.FirefoxService(executable_path=geckodriver_path)
+    driver = webdriver.Firefox(service=service, options=options)
+
+    driver.get(url)
+    try:
+        login_button = driver.find_element(By.LINK_TEXT, "登入")
+        login_button.click()
+        email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
+        password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
+        email_input.send_keys(os.getenv("CAKE_EMAIL"))
+        password_input.send_keys(os.getenv("CAKE_PASSWORD"))
+
+        submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+        submit_button.click()
+
+        time.sleep(5)
+
+        job_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'SidebarMenu_menuHeaderTitle__t1Ow3') and text()='求職']"))
+        )
+        job_menu.click()
+
+        applied_jobs = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'SidebarMenu_menuItemContentValue__MYWG2') and .//span[text()='已應徵職缺']]"))
+        )
+        applied_jobs.click()
+
+        time.sleep(5)
+
+        with open('response_content.html', 'w', encoding='utf-8') as file:
+            file.write(driver.page_source)
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        apply_jobs = soup.find_all('div', class_='UserJobApplicationList_item__za9o4')
+
+        jobs = []
+
+        for job in apply_jobs:
+            company_name = job.find("a", class_="JobApplicationItem_companyName__NXrvJ").text.strip()
+            job_title = job.find("a", class_="JobApplicationItem_jobTitle__i1m5a").text.strip()
+            company_url = job.find("a", class_="JobApplicationItem_companyName__NXrvJ")['href']
+            job_url = f"{url}{job.find("a", class_="JobApplicationItem_jobTitle__i1m5a")['href']}"
+            logo_url = job.find("img", class_="CompanyLogo_logo__1hyTe")['src']
+            cover_letter = job.find("div", class_="JobApplicationItem_coverLetterSingleLine__Ec31B").text.strip()
+
+            application_status = {
+                "read_within": job.find("div", class_="InlineMessage_inlineMessage____Ulc").text.strip(),
+                "employer_status": job.find_all("div", class_="InlineMessage_inlineMessage____Ulc")[1].text.strip(),
+                "resume_link": f"{url}{job.find("a", class_="JobApplicationItem_file__q5eyr")['href']}",
+                "days_ago": job.find("span", class_="JobApplicationItem_note__uSzuh").text.strip()
+            }
+
+            job_info = {
+                "id": str(uuid.uuid4()),
+                "company": company_name,
+                "job_title": job_title,
+                "logo_url": logo_url,
+                "company_url": company_url,
+                "job_url": job_url,
+                "cover_letter": cover_letter,
+                "application_status": application_status
+            }
+
+            jobs.append(job_info)
+
+        return JsonResponse(jobs, safe=False)
+
+    except TimeoutException as e:
+        return JsonResponse({"error": "操作超時，無法找到元素。"}, status=500)
+
+    except NoSuchElementException as e:
+        return JsonResponse({"error": "無法找到指定的元素。"}, status=500)
+
+    finally:
+        driver.quit()
